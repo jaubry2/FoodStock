ac6f2aa794f52a24fa5ec2e8c503c76ed55b3c79
jaubry2 <120647406+jaubry2@users.noreply.github.com>
2023-05-09 22:15:28 +0200

    Interface Graphique

---
+++ FS/src/FoodStock.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.foodstock;
+
+/**
+ *
+ * @author julesa
+ */
+public class FoodStock {
+    
+    public static void main(String[] args) {
+        Accueil mA = new Accueil();
+        mA.setVisible(true);
+    }
+}
