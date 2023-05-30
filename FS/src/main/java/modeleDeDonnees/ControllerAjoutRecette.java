4eb40ffe096264ee3f8da5a66b444d1907068de0
00marcito00 <129764842+00marcito00@users.noreply.github.com>
2023-05-30 10:22:50 +0200

    Update ControllerAjoutRecette.java

--- FS/src/main/java/modeleDeDonnees/ControllerAjoutRecette.java
+++ FS/src/main/java/modeleDeDonnees/ControllerAjoutRecette.java
@@ -13,13 +13,21 @@ import interfaceGraphique.AjouterRecette;
 import interfaceGraphique.Main;
 import interfaceGraphique.MenuListe;
 
+/**
+ * Contrôleur pour l'ajout d'une recette.
+ */
 public class ControllerAjoutRecette implements Observer {
 
     private LivreRecettes model;
     private AjouterRecette vue;
 
+    /**
+     * Constructeur du contrôleur pour l'ajout d'une recette.
+     *
+     * @param model le modèle de livre de recettes
+     * @param vue   la vue pour l'ajout de recette
+     */
     public ControllerAjoutRecette(LivreRecettes model, AjouterRecette vue) {
-        System.out.println("controlleur");
         this.model = model;
         this.vue = vue;
 
@@ -28,7 +36,6 @@ public class ControllerAjoutRecette implements Observer {
         vue.getAddLivreButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("recette boutton");
                 String nom = vue.getNomRecette().getText();
                 int duree = Integer.parseInt(vue.getDureeRecette().getText());
                 Set<Ingredient> ingredientsRecette = new HashSet<>();
@@ -46,49 +53,47 @@ public class ControllerAjoutRecette implements Observer {
         vue.getBoutonAjouter().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Ajoute une nouvelle ligne vide au modèle de table
-    	        String nomIngredient = vue.getNomIngredientTextField().getText();
-    	        String qteIngredient = vue.getQteTextField().getText();
-    	        String[] infoAliment = new String[3];
-    	        int result;
+                String nomIngredient = vue.getNomIngredientTextField().getText();
+                String qteIngredient = vue.getQteTextField().getText();
+                String[] infoAliment = new String[3];
+                int result;
 
-    	        if (Main.ensembleAliment.isAliment(nomIngredient)){
-    	            /* Ajout à la Table */
-    	            String[] ligne = new String[] {nomIngredient, qteIngredient};
-        	        vue.getdTMAliment().addRow(ligne);
-    	        } else{
-    	                infoAliment = Main.controller.popUpAliment(vue, nomIngredient);
-    	                result = JOptionPane.showConfirmDialog(vue, "Vous voulez ajoutez au stock : " + nomIngredient + " / " + infoAliment[0] + " / " + infoAliment[1] + " / " + infoAliment[2]);
-    	            
-    	            if (result == JOptionPane.YES_OPTION & !infoAliment[0].equals("") & !infoAliment[1].equals("") & !infoAliment[2].equals("")){
-    	                Aliment aliment = Main.controller.creerAliment(nomIngredient, UnitedeMesure.valueOf(infoAliment[0]), MoyendeConservation.valueOf(infoAliment[1]), TypeAliment.valueOf(infoAliment[2]));
-    	                Main.ensembleAliment.ajouterAliment(aliment);
-        	            String[] ligne = new String[] {nomIngredient, qteIngredient};
-            	        vue.getdTMAliment().addRow(ligne);
-    	                JOptionPane.showMessageDialog(vue, "L'aliment a été ajouté à l'ensemble d'aliment");
-    	            } else if(result == JOptionPane.NO_OPTION) {
-    	                JOptionPane.showMessageDialog(vue, "L'aliment n'a pas été ajouté à l'ensemble d'aliment");
-    	            }
-    	        }
-    	        
-    	        vue.getTableIngredientsRecette().setModel(vue.getdTMAliment());
-    	        
+                if (Main.ensembleAliment.isAliment(nomIngredient)){
+                    /* Ajout à la Table */
+                    String[] ligne = new String[] {nomIngredient, qteIngredient};
+                    vue.getdTMAliment().addRow(ligne);
+                } else {
+                    infoAliment = Main.controller.popUpAliment(vue, nomIngredient);
+                    result = JOptionPane.showConfirmDialog(vue, "Vous voulez ajoutez au stock : " + nomIngredient + " / " + infoAliment[0] + " / " + infoAliment[1] + " / " + infoAliment[2]);
+
+                    if (result == JOptionPane.YES_OPTION & !infoAliment[0].equals("") & !infoAliment[1].equals("") & !infoAliment[2].equals("")) {
+                        Aliment aliment = Main.controller.creerAliment(nomIngredient, UnitedeMesure.valueOf(infoAliment[0]), MoyendeConservation.valueOf(infoAliment[1]), TypeAliment.valueOf(infoAliment[2]));
+                        Main.ensembleAliment.ajouterAliment(aliment);
+                        String[] ligne = new String[] {nomIngredient, qteIngredient};
+                        vue.getdTMAliment().addRow(ligne);
+                        JOptionPane.showMessageDialog(vue, "L'aliment a été ajouté à l'ensemble d'aliment");
+                    } else if (result == JOptionPane.NO_OPTION) {
+                        JOptionPane.showMessageDialog(vue, "L'aliment n'a pas été ajouté à l'ensemble d'aliment");
+                    }
+                }
+
+                vue.getTableIngredientsRecette().setModel(vue.getdTMAliment());
             }
         });
+
         vue.getBoutonSupprimer().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-    	      
-    	        vue.getdTMAliment().removeRow(vue.getTableIngredientsRecette().getSelectedRow());
-
-    	        
+                vue.getdTMAliment().removeRow(vue.getTableIngredientsRecette().getSelectedRow());
             }
         });
 
     }
 
+    /**
+     * Méthode appelée lors de la mise à jour du modèle.
+     */
     public void update() {
-        System.out.println("update");
         Main.controller.changerPage("Menu Recette", null, null, null);
     }
 }
