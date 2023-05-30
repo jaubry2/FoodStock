4e36f000e6a854bc181fbbb4d8f044e3050bbd28
00marcito00 <129764842+00marcito00@users.noreply.github.com>
2023-05-30 10:19:39 +0200

    Update AlimentsNonPresentException.java

--- FS/src/main/java/modeleDeDonnees/AlimentsNonPresentException.java
+++ FS/src/main/java/modeleDeDonnees/AlimentsNonPresentException.java
@@ -1,6 +1,15 @@
 package modeleDeDonnees;
 
+/**
+ * Exception levée lorsque l'aliment recherché n'est pas présent.
+ */
 public class AlimentsNonPresentException extends RuntimeException {
+
+    /**
+     * Constructeur de l'exception.
+     *
+     * @param nomAliment le nom de l'aliment non présent
+     */
     public AlimentsNonPresentException(String nomAliment) {
         super("L'aliment '" + nomAliment + "' n'est pas présent.");
     }
