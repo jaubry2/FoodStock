d85218ccd27586dfab1660ea0719db923cc03b83
00marcito00 <129764842+00marcito00@users.noreply.github.com>
2023-05-30 10:28:14 +0200

    Update CritereDureeRecette.java

--- FS/src/main/java/modeleDeDonnees/CritereDureeRecette.java
+++ FS/src/main/java/modeleDeDonnees/CritereDureeRecette.java
@@ -6,23 +6,47 @@ package modeleDeDonnees;
  */
 public enum CritereDureeRecette {
     RAPIDE() {
+        /**
+         * Vérifie si la valeur donnée est dans la plage pour le critère RAPIDE.
+         *
+         * @param valeur la valeur à vérifier
+         * @return true si la valeur est inférieure à 15, sinon false
+         */
         @Override
         public boolean estDansLaPlage(int valeur) {
             return valeur < 15;
         }
     },
     MEDIUM() {
+        /**
+         * Vérifie si la valeur donnée est dans la plage pour le critère MEDIUM.
+         *
+         * @param valeur la valeur à vérifier
+         * @return true si la valeur est comprise entre 15 et 30 (inclus), sinon false
+         */
         @Override
         public boolean estDansLaPlage(int valeur) {
             return valeur >= 15 && valeur <= 30;
         }
     },
     LONG() {
+        /**
+         * Vérifie si la valeur donnée est dans la plage pour le critère LONG.
+         *
+         * @param valeur la valeur à vérifier
+         * @return true si la valeur est supérieure à 30, sinon false
+         */
         @Override
         public boolean estDansLaPlage(int valeur) {
             return valeur > 30;
         }
     };
 
+    /**
+     * Vérifie si la valeur donnée est dans la plage correspondant au critère.
+     *
+     * @param valeur la valeur à vérifier
+     * @return true si la valeur est dans la plage, sinon false
+     */
     public abstract boolean estDansLaPlage(int valeur);
 }
