edbf95721ed9a8f0da2f779dcfbf7279c27a4db1
00marcito00 <129764842+00marcito00@users.noreply.github.com>
2023-05-30 09:39:50 +0200

    Update Ingredient.java

--- FS/src/main/java/modeleDeDonnees/Ingredient.java
+++ FS/src/main/java/modeleDeDonnees/Ingredient.java
@@ -10,6 +10,9 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
+/**
+ * Classe représentant un ingrédient utilisé dans une recette.
+ */
 @Entity
 @Table(name = "ingredient")
 public class Ingredient {
@@ -17,52 +20,95 @@ public class Ingredient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    /**
+     * Aliment associé à l'ingrédient.
+     */
     @OneToOne
     @JoinColumn(name = "idAliment", referencedColumnName = "id")
     private Aliment aliment;
 
+    /**
+     * Quantité de l'ingrédient utilisée dans la recette.
+     */
     @Column(name = "quantite")
     private float quantite;
-    
 
-    
-	@Column(name = "recette_id")
-	private Long recette_id;
+    /**
+     * Identifiant de la recette à laquelle l'ingrédient appartient.
+     */
+    @Column(name = "recette_id")
+    private Long recette_id;
 
-  
+    /**
+     * Constructeur par défaut de la classe Ingredient.
+     */
     public Ingredient() {
     }
 
-    public Ingredient( Aliment aliment, float quantite) {
+    /**
+     * Constructeur de la classe Ingredient avec les paramètres spécifiés.
+     *
+     * @param aliment  L'aliment associé à l'ingrédient.
+     * @param quantite La quantité de l'ingrédient.
+     */
+    public Ingredient(Aliment aliment, float quantite) {
         this.aliment = aliment;
         this.quantite = quantite;
     }
 
-	public long getId() {
-		return id;
-	}
+    /**
+     * Retourne l'identifiant de l'ingrédient.
+     *
+     * @return L'identifiant de l'ingrédient.
+     */
+    public long getId() {
+        return id;
+    }
 
-	public Aliment getAliment() {
-		return aliment;
-	}
+    /**
+     * Retourne l'aliment associé à l'ingrédient.
+     *
+     * @return L'aliment associé à l'ingrédient.
+     */
+    public Aliment getAliment() {
+        return aliment;
+    }
 
-	public void setQuantite(float nouvelleQuantite) {
-		this.quantite=nouvelleQuantite;
-	}
+    /**
+     * Modifie la quantité de l'ingrédient.
+     *
+     * @param nouvelleQuantite La nouvelle quantité de l'ingrédient.
+     */
+    public void setQuantite(float nouvelleQuantite) {
+        this.quantite = nouvelleQuantite;
+    }
 
-	public float  getQuantite() {
-		return quantite;
-	}
+    /**
+     * Retourne la quantité de l'ingrédient.
+     *
+     * @return La quantité de l'ingrédient.
+     */
+    public float getQuantite() {
+        return quantite;
+    }
 
-	public boolean isAliment(Aliment aliment) {
-		return this.aliment.equals(aliment);
-		
-	}
-	public boolean equals(Ingredient ingredientComparaison) {
-		return this.getAliment().equals(ingredientComparaison.getAliment());
-	}
+    /**
+     * Vérifie si l'ingrédient est égal à l'aliment spécifié.
+     *
+     * @param aliment L'aliment à comparer.
+     * @return Vrai si l'ingrédient est égal à l'aliment spécifié, sinon faux.
+     */
+    public boolean isAliment(Aliment aliment) {
+        return this.aliment.equals(aliment);
+    }
 
-
+    /**
+     * Vérifie si l'ingrédient est égal à l'ingrédient de comparaison spécifié.
+     *
+     * @param ingredientComparaison L'ingrédient de comparaison.
+     * @return Vrai si l'ingrédient est égal à l'ingrédient de comparaison, sinon faux.
+     */
+    public boolean equals(Ingredient ingredientComparaison) {
+        return this.getAliment().equals(ingredientComparaison.getAliment());
+    }
 }
-
