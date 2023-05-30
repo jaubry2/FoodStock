b74e604a467ce934af4514c1b27297dd4dc69a5f
00marcito00 <129764842+00marcito00@users.noreply.github.com>
2023-05-30 10:45:25 +0200

    Update Observer.java

--- FS/src/main/java/modeleDeDonnees/Observer.java
+++ FS/src/main/java/modeleDeDonnees/Observer.java
@@ -2,6 +2,10 @@ package modeleDeDonnees;
 
 public interface Observer {
     
+    /**
+     * Méthode appelée lorsqu'un changement est notifié à l'observateur.
+     * Cette méthode doit être implémentée par les classes qui souhaitent observer un sujet.
+     */
     public void update();
     
 }
