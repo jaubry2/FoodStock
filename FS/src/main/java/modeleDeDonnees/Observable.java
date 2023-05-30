b38d1189d9df8b500794a499f2e5cd89feb5cd24
00marcito00 <129764842+00marcito00@users.noreply.github.com>
2023-05-30 10:44:16 +0200

    Update Observable.java

--- FS/src/main/java/modeleDeDonnees/Observable.java
+++ FS/src/main/java/modeleDeDonnees/Observable.java
@@ -1,6 +1,5 @@
 package modeleDeDonnees;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,17 +7,22 @@ public class Observable {
 
     List<Observer> observers = new ArrayList<Observer>();
 
+    /**
+     * Ajoute un observateur à la liste des observateurs.
+     *
+     * @param obs l'observateur à ajouter
+     */
     public void addObserver(Observer obs) {
         observers.add(obs);
     }
 
+    /**
+     * Notifie tous les observateurs en appelant leur méthode update().
+     */
     public void notifyObservers() {
         for (Observer obs : observers) {
             obs.update();
         }
     }
 
-    
-    
 }
-
