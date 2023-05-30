dba6dde47470c46240c06de794ee674ad24c216c
00marcito00 <129764842+00marcito00@users.noreply.github.com>
2023-05-30 10:50:53 +0200

    Update RecetteNonPresenteException.java

--- FS/src/main/java/modeleDeDonnees/RecetteNonPresenteException.java
+++ FS/src/main/java/modeleDeDonnees/RecetteNonPresenteException.java
@@ -1,7 +1,16 @@
 package modeleDeDonnees;
 
+/**
+ * Exception levée lorsque la recette n'est pas présente.
+ */
 public class RecetteNonPresenteException extends RuntimeException {
-    public RecetteNonPresenteException(String nomAliment) {
-        super("La recette '" + nomAliment + "' n'est pas présente.");
+
+    /**
+     * Constructeur de l'exception avec le nom de la recette manquante.
+     *
+     * @param nomRecette Le nom de la recette manquante.
+     */
+    public RecetteNonPresenteException(String nomRecette) {
+        super("La recette '" + nomRecette + "' n'est pas présente.");
     }
 }
